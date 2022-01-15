10d7fffbdacac057138ce9f53118ef143d7b98f2
Derfellios <joris.nieuwveld@gmail.com>
2022-01-15 22:48:24 +0100

    Update Client_GameRefresh.lua

--- Client_GameRefresh.lua
+++ Client_GameRefresh.lua
@@ -1,11 +1,8 @@
 function Client_GameRefresh(game)
-
 	if game.Us ~= nil and (not Mod.PlayerGameData.InitialPopupDisplayed and not (game.Game.TurnNumber > 1 or (game.Game.TurnNumber == 1 and not game.Settings.AutomaticTerritoryDistribution))) then
-		UI.Alert("This game includes the Landriamat mod. Landria rules apply! So don't have too many borders!")
+		UI.Alert("This game includes the Landria Maker mod. Landria rules apply! So don't have too many borders!")
 		local payload = {};
 		payload.Message = "InitialPopupDisplayed";
 		game.SendGameCustomMessage("Please wait... ", payload, function(reply)end);
 	end
-
-
 end
